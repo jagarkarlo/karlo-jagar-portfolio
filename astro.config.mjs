ef7f0ffaf8e937f1f2d17eb197ddab6d318725f0
Karlo Jagar <94485299+jagarkarlo@users.noreply.github.com>
2026-08-19 09:23:49 +0200

    build: add icon assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://jagarkarlo.github.io",
   base: "/karlo-jagar-portfolio",
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
