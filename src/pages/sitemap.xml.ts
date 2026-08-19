c825881011e984c8728ec18436f4cfc6cf6901bf
Karlo Jagar <94485299+jagarkarlo@users.noreply.github.com>
2026-08-19 08:18:50 +0200

    seo: include portfolio routes

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,10 +2,11 @@ import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({ site }) => {
   const base = import.meta.env.BASE_URL.replace(/\/$/, "");
-  const location = new URL(`${base}/`, site);
+  const routes = ["/", "/about/", "/work/", "/contact/"];
+  const locations = routes.map((route) => new URL(`${base}${route}`, site));
   const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>${location}</loc></url>
+${locations.map((location) => `  <url><loc>${location}</loc></url>`).join("\n")}
 </urlset>`;
 
   return new Response(body, {
