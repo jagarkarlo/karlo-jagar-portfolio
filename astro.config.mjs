2bc610b1e625abafa5aa9c3f0195baba25603f25
Karlo Jagar <94485299+jagarkarlo@users.noreply.github.com>
2026-08-19 08:41:19 +0200

    build: enable Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://jagarkarlo.github.io",
   base: "/karlo-jagar-portfolio",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
